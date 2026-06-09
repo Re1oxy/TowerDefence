c478ba61d674632914d497c9787605306052448b
Re1oxy <artyrawanhot@gmail.com>
2026-06-09 11:08:48 +0200

    Add entity for future towers bullets

---
+++ Models/Entity.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace TowerDefense.Models
+{
+    /// <summary>
+    /// Basic abstract class for all game entities.
+    /// </summary>
+    public abstract class Entity
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public int Width { get; protected set; }
+        public int Height { get; protected set; }
+        public bool IsAlive { get; protected set; } = true;
+
+        protected Entity(float x, float y, int width, int height)
+        {
+            X = x; Y = y; Width = width; Height = height;
+        }
+
+        public RectangleF Bounds => new RectangleF(X, Y, Width, Height);
+
+        public abstract void Update(float deltaTime);
+        public abstract void Draw(Graphics g);
+    }
+}
+
